b82a967fef694755605ddaae1499da771979ea23
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-08-22 02:28:22 +0200

    tired of all the troubles with poltergeist/capybara-webkit - use selenium

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,7 +2,6 @@ require 'factory_girl'
 require 'database_cleaner'
 require 'database_cleaner/cucumber'
 require 'capybara/cucumber'
-require 'capybara/webkit'
 require 'capybara-screenshot/cucumber'
 require 'exhaust'
 
@@ -18,17 +17,24 @@ Capybara.save_path = File.expand_path('../../../screenshots', __FILE__)
 # Database Cleaner to clear out the test DB between tests
 DatabaseCleaner.strategy = :truncation
 
-Exhaust.run!
-at_exit { Exhaust.shutdown! }
-
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
 
 Capybara.configure do |config|
   # Use whatever driver you want
   config.app_host = Exhaust.ember_host
-  config.default_driver = :webkit
+  config.default_driver = :selenium
 end
 
+After do
+  Capybara.reset_sessions!
+  visit '/'
+  page.execute_script("localStorage.clear()")
+end
 
 # Shorthand FactoryGirl
 include FactoryGirl::Syntax::Methods
 
+Exhaust.run!
+at_exit { Exhaust.shutdown! }
