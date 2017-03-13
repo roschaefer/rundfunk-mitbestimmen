73114f1a8aaaa59bbd02ed08a1b40e75451f9ccf
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-03-14 00:08:24 +0100

    simplify .travis.yml add deploy task and encrypted deploy key update capistrano

--- backend/config/deploy.rb
+++ backend/config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.7.1'
+lock '3.8.0'
 
 set :application, 'rundfunk-backend'
 set :repo_url, 'https://github.com/roschaefer/rundfunk-backend.git'
