1795ea2c62754f75e1ffc393688522e7e7579a3c
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-03-18 14:51:46 +0100

    fix tests

--- frontend/tests/unit/routes/data-privacy-test.js
+++ frontend/tests/unit/routes/data-privacy-test.js
@@ -3,6 +3,10 @@ import { moduleFor, test } from 'ember-qunit';
 moduleFor('route:data-privacy', 'Unit | Route | data privacy', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
+  needs: [
+    'service:metrics',
+    'ember-metrics@metrics-adapter:piwik', // bundled adapter
+  ]
 });
 
 test('it exists', function(assert) {
