95d6596e07adfd29ac6d769c70af868d74f4cffa
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-03-23 22:49:46 +0100

    move controller logic into route to call sendAction

--- frontend/app/controllers/application.js
+++ frontend/app/controllers/application.js
@@ -2,35 +2,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  intl: Ember.inject.service(),
-  session: Ember.inject.service('session'),
-  actions: {
-    login (afterLoginRoute) {
-      const lang = this.get('intl').get('locale')[0];
-      // Check out the docs for all the options:
-      // https://auth0.com/docs/libraries/lock/customization
-      const lockOptions = {
-        theme: {
-          logo:  'https://rundfunk-mitbestimmen.de/assets/images/logo.png'
-        },
-        language: lang,
-        auth: {
-          autoclose: true,
-          redirect: false,
-          responseType: 'token',
-          redirectUrl: 'http://localhost:4200/login',
-          params: {
-            state: (afterLoginRoute || this.get('router.url')),
-            scope: 'openid email',
-          }
-        }
-      };
-
-      this.get('session').authenticate('authenticator:auth0-lock', lockOptions);
-    },
-
-    logout () {
-      this.get('session').invalidate();
-    }
-  }
+  session: Ember.inject.service('session')
 });
--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -21,6 +21,35 @@ export default Ember.Route.extend(ApplicationRouteMixin , {
     // The primary usecase is if you side load all translations
     //
     // return this.get('intl').setLocale(['en-ca', 'en-us']);
+  },
+  actions: {
+    login (afterLoginRoute) {
+      const lang = this.get('intl').get('locale')[0];
+      // Check out the docs for all the options:
+      // https://auth0.com/docs/libraries/lock/customization
+      const lockOptions = {
+        theme: {
+          logo:  'https://rundfunk-mitbestimmen.de/assets/images/logo.png'
+        },
+        language: lang,
+        auth: {
+          autoclose: true,
+          redirect: false,
+          responseType: 'token',
+          redirectUrl: 'http://localhost:4200/login',
+          params: {
+            state: (afterLoginRoute || this.get('router.url')),
+            scope: 'openid email',
+          }
+        }
+      };
+
+      this.get('session').authenticate('authenticator:auth0-lock', lockOptions);
+    },
+
+    logout () {
+      this.get('session').invalidate();
+    }
   }
 });
 
