aff7defe9eccf7140b0e79b121d51827ee655e34
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-03-28 01:26:20 +0200

    fix the awful async problem during authentication

--- frontend/app/routes/login.js
+++ frontend/app/routes/login.js
@@ -8,7 +8,7 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
       window._paq.push(['trackGoal', 4]);
       //goalId 4 is "Signup/Login successful"
     }
-    this.store.findRecord('condensed-balance', 0).then(() => {
+    this.store.findRecord('condensed-balance', 0, {reload: true}).then(() => {
       // make a random request to the database, the user will be saved to the
       // datatabase and all subsequent request will be associated to that user
       return Ember.RSVP.allSettled(this.store.peekAll('selection').map((s) => {
