fc37c9915d9896b81f516e4f813c73eb7f60e162
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-03-28 23:45:44 +0200

    fix stub-authenticator

--- frontend/app/authenticators/stub-authenticator.js
+++ frontend/app/authenticators/stub-authenticator.js
@@ -2,11 +2,20 @@ import Ember from 'ember';
 import Base from 'ember-simple-auth/authenticators/base';
 
 export default Base.extend({
+  restore(data) {
+    return Ember.RSVP.resolve(data);
+  },
   authenticate(options) {
     return new Ember.RSVP.Promise((resolve) => {
       const idToken = window.stubbedJwt;
       const state = options.auth.params.state;
-      const sessionData = {idToken, state };
+      const sessionData =  {
+        idToken,
+        idTokenPayload: {
+          "exp": 42, // the authorizer just wants a number
+        },
+        state
+      };
       resolve(sessionData);
     });
   },
