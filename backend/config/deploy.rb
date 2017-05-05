6291b46efc4267d59db60293b4c583514e420cf0
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-05-05 13:39:12 +0200

    update capistrano

--- backend/config/deploy.rb
+++ backend/config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.8.0'
+lock '3.8.1'
 
 set :application, 'rundfunk-backend'
 set :repo_url, 'https://github.com/roschaefer/rundfunk-mitbestimmen.git'
