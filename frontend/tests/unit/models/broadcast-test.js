5afaca62154776c5b292b4d734d8a76a15d559f0
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-06-01 21:11:09 +0200

    fixed broadcast model test

--- frontend/tests/unit/models/broadcast-test.js
+++ frontend/tests/unit/models/broadcast-test.js
@@ -1,111 +1,107 @@
-import { moduleForModel, test } from 'ember-qunit';
-import { make } from 'ember-data-factory-guy';
+/* jshint expr: true */
 import Ember from 'ember';
-import startApp from '../../helpers/start-app';
+import { expect } from 'chai';
+import { beforeEach, describe, it } from 'mocha';
+import { setupModelTest } from 'ember-mocha';
+import { make, manualSetup } from 'ember-data-factory-guy';
 
-let App;
+describe('Unit | Model | broadcast', function() {
+  setupModelTest('broadcast', {
+    // Specify the other units that are required for this it.
+    needs: ['model:selection', 'model:medium', 'model:station']
+  });
+  beforeEach(function() {
+    manualSetup(this.container);
+  });
 
-moduleForModel('broadcast', 'Unit | Model | broadcast', {
-  // Specify the other units that are required for this test.
-  setup: function() {
+  // Replace this with your real tests.
+  it('exists', function() {
+    let model = this.subject();
+    // var store = this.store();
+    expect(model).to.be.ok;
+  });
+
+  it('respond response must be "positive" or "negative"', function() {
+    let model = this.subject();
+    let selection = model.respond('foobar');
+    expect(selection).to.be.undefined;
+    expect(model.get('selections').get('length')).to.eq(0);
+  });
+
+  it('respond returns a new selection with the response', function() {
+    let model = this.subject();
+    let selection = model.respond('positive');
+    expect(selection.get('response')).to.eq('positive');
+  });
+
+  it('respond adds a new selection to the broadcast', function() {
+    let model = this.subject();
+    model.respond('positive');
+    expect(model.get('selections').get('length')).to.eq(1);
+  });
+
+  it('respond updates the current selection if any', function() {
+    let model = this.subject();
+    let selection = make('selection', {response: 'neutral'});
     Ember.run(function() {
-      App = startApp();
+      model.set('selections', [selection]);
+      model.respond('positive');
     });
-  },
-  teardown: function() {
+    expect(selection.get('response')).to.eq('positive');
+  });
+
+  it('respond updates and does not create more than one selection', function() {
+    let model = this.subject();
+    let selection = make('selection', {response: 'neutral'});
     Ember.run(function() {
-      App.destroy();
+      model.set('selections', [selection]);
+      expect(model.get('selections').get('length')).to.eq(1);
+      model.respond('positive');
     });
-  },
-  needs: ['model:topic', 'model:format', 'model:selection', 'model:medium', 'model:station']
-});
-
-test('it exists', function(assert) {
-  let model = this.subject();
-  // let store = this.store();
-  assert.ok(!!model);
-});
-
-test('respond response must be "positive" or "negative"', function(assert) {
-  let model = this.subject();
-  let selection = model.respond('foobar');
-  assert.equal(selection, null);
-  assert.equal(model.get('selections').get('length'), 0);
-});
-
-test('respond returns a new selection with the response', function(assert) {
-  let model = this.subject();
-  let selection = model.respond('positive');
-  assert.equal(selection.get('response'), 'positive');
-});
-
-test('respond adds a new selection to the broadcast', function(assert) {
-  let model = this.subject();
-  model.respond('positive');
-  assert.equal(model.get('selections').get('length'), 1);
-});
-
-test('respond updates the current selection if any', function(assert) {
-  let model = this.subject();
-  let selection = make('selection', {response: 'neutral'});
-  Ember.run(function() {
-    model.set('selections', [selection]);
-    model.respond('positive');
+    expect(model.get('selections').get('length')).to.eq(1);
   });
-  assert.equal(selection.get('response'), 'positive');
-});
 
-test('respond updates and does not create more than one selection', function(assert) {
-  let model = this.subject();
-  let selection = make('selection', {response: 'neutral'});
-  Ember.run(function() {
-    model.set('selections', [selection]);
-    assert.equal(model.get('selections').get('length'), 1);
-    model.respond('positive');
+  it('respond "neutral" clears the amount', function() {
+    let model = this.subject();
+    let selection = make('selection', {
+      response: 'neutral',
+      amount: 5.0,
+    });
+    Ember.run(function() {
+      model.set('selections', [selection]);
+      model.respond('neutral');
+      expect(selection.get('response')).to.eq('neutral');
+      expect(selection.get('amount')).to.eq(null);
+    });
   });
-  assert.equal(model.get('selections').get('length'), 1);
-});
 
-test('respond "neutral" clears the amount', function(assert) {
-  let model = this.subject();
-  let selection = make('selection', {
-    response: 'neutral',
-    amount: 5.0,
+  it('respond "neutral" will also unfix the amount', function() {
+    let model = this.subject();
+    let selection = make('selection', {
+      response: 'positive',
+      fixed: true,
+      amount: 5.0,
+    });
+    Ember.run(function() {
+      model.set('selections', [selection]);
+      model.respond('neutral');
+      expect(selection.get('response')).to.eq('neutral');
+      expect(selection.get('amount')).to.eq(null);
+      expect(selection.get('fixed')).to.eq(false);
+    });
   });
-  Ember.run(function() {
-    model.set('selections', [selection]);
-    model.respond('neutral');
-    assert.equal(selection.get('response'), 'neutral');
-    assert.equal(selection.get('amount'), null);
-  });
-});
 
-test('respond "neutral" will also unfix the amount', function(assert) {
-  let model = this.subject();
-  let selection = make('selection', {
-    response: 'positive',
-    fixed: true,
-    amount: 5.0,
-  });
-  Ember.run(function() {
-    model.set('selections', [selection]);
-    model.respond('neutral');
-    assert.equal(selection.get('response'), 'neutral');
-    assert.equal(selection.get('amount'), null);
-    assert.equal(selection.get('fixed'), false);
-  });
-});
-
-test('respond "positive" keeps the amount', function(assert) {
-  let model = this.subject();
-  let selection = make('selection', {
-    response: 'positive',
-    amount: 5.0,
-  });
-  Ember.run(function() {
-    model.set('selections', [selection]);
-    model.respond('positive');
-    assert.equal(selection.get('response'), 'positive');
-    assert.equal(selection.get('amount'), 5.0);
+  it('respond "positive" keeps the amount', function() {
+    let model = this.subject();
+    let selection = make('selection', {
+      response: 'positive',
+      amount: 5.0,
+    });
+    Ember.run(function() {
+      model.set('selections', [selection]);
+      model.respond('positive');
+      expect(selection.get('response')).to.eq('positive');
+      expect(selection.get('amount')).to.eq(5.0);
+    });
   });
 });
