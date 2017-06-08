34813244678f0a2fcbfba8030a322c5d6b67a54f
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-06-08 06:16:15 +0200

    bugfix and refactoring: call broadcast.response in decision-card template

--- frontend/app/components/decision-card.js
+++ frontend/app/components/decision-card.js
@@ -2,16 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['decision-card ui fluid card'],
-  response: 'neutral',
 
-  didReceiveAttrs() {
-    this._super(...arguments);
-    this.set('response', this.get('broadcast.response'));
-  },
   actions: {
     respond(response){
       this.get('broadcast').respond(response);
-      this.set('response', this.get('broadcast.response'));
       this.rerender();
     }
   }
