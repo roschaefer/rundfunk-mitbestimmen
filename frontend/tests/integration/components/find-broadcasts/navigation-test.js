cd42fdd032e64578413679dc3dcadd4630b0896c
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-06-09 21:58:31 +0200

    implement more subtle suggestion to distribute budget

--- frontend/tests/integration/components/find-broadcasts/navigation-test.js
+++ frontend/tests/integration/components/find-broadcasts/navigation-test.js
@@ -4,7 +4,7 @@ import { context, beforeEach, describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 
-let step;
+let step, recentPositives;
 
 describe('Integration | Component | find broadcasts/navigation', function() {
   setupComponentTest('find-broadcasts/navigation', {
@@ -14,6 +14,56 @@ describe('Integration | Component | find broadcasts/navigation', function() {
   it('renders', function() {
     this.render(hbs`{{find-broadcasts/navigation}}`);
     expect(this.$()).to.have.length(1);
+  });
+
+  describe('heart label', function() {
+    context('recentPositives = 0', function() {
+      beforeEach(function(){
+        recentPositives = 0;
+      });
+
+      it('shows grey heart', function() {
+        this.set('recentPositives', recentPositives);
+        this.render(hbs`{{find-broadcasts/navigation step=1 totalSteps=10 browse=browse recentPositives=recentPositives}}`);
+        expect(this.$('.heart.icon').hasClass('red')).to.be.false;
+      });
+
+      it('does not suggest to distribute the budget', function() {
+        this.set('recentPositives', recentPositives);
+        this.render(hbs`{{find-broadcasts/navigation step=1 totalSteps=10 browse=browse recentPositives=recentPositives}}`);
+        expect(this.$('.find-broadcasts-navigation-distribute-button').hasClass('primary')).to.be.false;
+      });
+    });
+
+    context('recentPositives = 1', function() {
+      beforeEach(function(){
+        recentPositives = 1;
+      });
+
+      it('shows red heart', function() {
+        this.set('recentPositives', recentPositives);
+        this.render(hbs`{{find-broadcasts/navigation step=1 totalSteps=10 browse=browse recentPositives=recentPositives}}`);
+        expect(this.$('.heart.icon').hasClass('red')).to.be.true;
+      });
+    });
+
+    context('recentPositives = 3', function() {
+      beforeEach(function(){
+        recentPositives = 3;
+      });
+
+      it('shows red heart', function() {
+        this.set('recentPositives', recentPositives);
+        this.render(hbs`{{find-broadcasts/navigation step=1 totalSteps=10 browse=browse recentPositives=recentPositives}}`);
+        expect(this.$('.heart.icon').hasClass('red')).to.be.true;
+      });
+
+      it('suggests to distribute the budget', function() {
+        this.set('recentPositives', recentPositives);
+        this.render(hbs`{{find-broadcasts/navigation step=1 totalSteps=10 browse=browse recentPositives=recentPositives}}`);
+        expect(this.$('.find-broadcasts-navigation-distribute-button').hasClass('primary')).to.be.true;
+      });
+    });
   });
 
   context('step=1', function() {
