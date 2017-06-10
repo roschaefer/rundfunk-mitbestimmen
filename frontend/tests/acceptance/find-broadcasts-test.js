5210f07e07f3b7bf743b4aeeb876bfd29bcfc173
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-06-10 20:46:20 +0200

    `ember g acceptance-test find-broadcasts`

---
+++ frontend/tests/acceptance/find-broadcasts-test.js
@@ -0,0 +1,24 @@
+import { describe, it, beforeEach, afterEach } from 'mocha';
+import { expect } from 'chai';
+import startApp from '../helpers/start-app';
+import destroyApp from '../helpers/destroy-app';
+
+describe('Acceptance | find broadcasts', function() {
+  let application;
+
+  beforeEach(function() {
+    application = startApp();
+  });
+
+  afterEach(function() {
+    destroyApp(application);
+  });
+
+  it('can visit /find-broadcasts', function() {
+    visit('/find-broadcasts');
+
+    return andThen(() => {
+      expect(currentURL()).to.equal('/find-broadcasts');
+    });
+  });
+});
