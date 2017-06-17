beb05ec0247aa4ef7ed534ff1d171389359cf8e0
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-06-17 19:13:46 +0200

    frontend: show markers on a map

--- frontend/app/routes/visualize/geo.js
+++ frontend/app/routes/visualize/geo.js
@@ -2,4 +2,7 @@ import Ember from 'ember';
 import ResetScrollPositionMixin from 'frontend/mixins/reset-scroll-position';
 
 export default Ember.Route.extend(ResetScrollPositionMixin, {
+  model() {
+    return this.store.findAll('chart-data/geo/marker');
+  }
 });
