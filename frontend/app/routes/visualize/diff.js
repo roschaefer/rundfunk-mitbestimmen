07a27ade54deb044fa3d653dad7c7d6ced01fb14
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-06-22 00:25:09 +0200

    reset scroll mixin in /visualize

--- frontend/app/routes/visualize/diff.js
+++ frontend/app/routes/visualize/diff.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import ResetScrollPositionMixin from 'frontend/mixins/reset-scroll-position';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ResetScrollPositionMixin, {
   model() {
     return this.store.findRecord('chart-data/diff', 0);
   }
--- frontend/app/routes/visualize/index.js
+++ frontend/app/routes/visualize/index.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import ResetScrollPositionMixin from 'frontend/mixins/reset-scroll-position';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ResetScrollPositionMixin, {
 });
