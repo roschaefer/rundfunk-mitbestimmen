da8aecc316e58ab1d6bc69cb3f269d720cd5fcaf
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-06-24 12:33:30 +0200

    mount sidekiq web at /sidekiq

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   get 'statistics/', to: 'statistics#index'
   get 'summarized_statistics/:id', to: 'statistics#summarized'
@@ -11,4 +13,17 @@ Rails.application.routes.draw do
 
   get 'chart_data/diffs/:id', to: 'chart_data#diff'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  if Rails.env.production?
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      # Protect against timing attacks:
+      # - See https://codahale.com/a-lesson-in-timing-attacks/
+      # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+      # - Use & (do not use &&) so that it doesn't short circuit.
+      # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
+        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
+    end
+  end
+  mount Sidekiq::Web, at: '/sidekiq'
 end
