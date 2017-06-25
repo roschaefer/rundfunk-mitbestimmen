8e9d693c89876e487320ff1168c9eb3f7943569e
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-06-25 17:20:02 +0200

    fix depreaction 'ember-cli-babel'

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -4,7 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
-    babel: {
+    "ember-cli-babel": {
       includePolyfill: true
     },
     emberHighCharts: {
