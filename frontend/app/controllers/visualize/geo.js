af033e6e1bea6ab13b7ec141f950594e599b3b03
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-07-05 23:54:00 +0200

    implement chloropleth map

--- frontend/app/controllers/visualize/geo.js
+++ frontend/app/controllers/visualize/geo.js
@@ -1,8 +1,24 @@
 import Ember from 'ember';
+import chroma from 'chroma';
+
 
 export default Ember.Controller.extend({
-  lat: 51.520008,
+  lat: 51.3,
   lng: 10,
   zoom: 6,
-  tileLayerUrl: 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
+  tileLayerUrl: 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+  scale: chroma.scale(['yellow', 'red', 'black']),
+
+  actions: {
+    style(feature) {
+      return {
+        fillColor: this.get('scale')(feature.properties.user_count_fraction).hex(),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+      }
+    }
+  }
 });
--- frontend/app/routes/visualize/geo.js
+++ frontend/app/routes/visualize/geo.js
@@ -1,8 +1,12 @@
 import Ember from 'ember';
 import ResetScrollPositionMixin from 'frontend/mixins/reset-scroll-position';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend(ResetScrollPositionMixin, {
   model() {
-    return this.store.findAll('chart-data/geo/marker');
+    return RSVP.hash({
+      markers: this.store.findAll('chart-data/geo/marker'),
+      geojson: Ember.$.get('http://localhost:3000/chart_data/geo/geojson')
+    });
   }
 });
