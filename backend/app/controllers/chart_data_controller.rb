eba63444ebb2d4dfb5e6cdf1a3abb8f96b020259
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-07-07 15:32:20 +0200

    testing + refactoring geojson route

--- backend/app/controllers/chart_data_controller.rb
+++ backend/app/controllers/chart_data_controller.rb
@@ -32,15 +32,15 @@ class ChartDataController < ApplicationController
   end
 
   def geojson
-    state_codes = [ 'BW', 'BY', 'BE', 'BB', 'HB', 'HH', 'HE', 'MV', 'NI', 'NW', 'RP', 'SL', 'ST', 'SN', 'SH', 'TH' ]
-    users = User.where(country_code: 'DE', state_code: state_codes)
     template_feature_collection = RGeo::GeoJSON.decode(File.read(File.join(Rails.root, 'public', 'bundeslaender.geojson')), json_parser: :json)
-    feature_array = template_feature_collection.collect do |bundesland|
-      properties = bundesland.properties
-      users_in_bundesland = users.where(state_code: state_codes[bundesland.feature_id])
-      properties['user_count_total'] = users_in_bundesland.count
-      properties['user_count_fraction'] = (users_in_bundesland.count.to_f/users.count.to_f)
-      RGeo::GeoJSON::Feature.new(bundesland.geometry, bundesland.feature_id, properties)
+    state_codes = template_feature_collection.collect {|feature| feature.properties['state_code'] }
+    users = User.where(country_code: 'DE', state_code: state_codes)
+    feature_array = template_feature_collection.collect do |feature|
+      properties = feature.properties
+      users_in_state = users.where(state_code: properties['state_code'])
+      properties['user_count_total'] = users_in_state.count
+      properties['user_count_normalized'] = (users_in_state.count.to_f / users.count.to_f)
+      RGeo::GeoJSON::Feature.new(feature.geometry, feature.feature_id, properties)
     end
     render json: RGeo::GeoJSON.encode(RGeo::GeoJSON::FeatureCollection.new(feature_array))
   end
---
+++ backend/spec/requests/chart_data/chart_data_geo_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+RSpec.describe 'ChartData', type: :request do
+  let(:headers) { {} }
+  let(:params)  { {} }
+  let(:action) { get url, params: params, headers: headers }
+  let(:geojson) { RGeo::GeoJSON.decode(response.body, json_parser: :json) }
+  let(:state_codes) { %w[BW BY BE BB HB HH HE MV NI NW RP SL ST SN SH TH] }
+
+  describe 'GET' do
+    subject do
+      action
+      geojson
+    end
+
+    describe 'chart_data/geo/geojson' do
+      let(:url) { '/chart_data/geo/geojson' }
+      it 'returns state codes for all federal states' do
+        response_state_codes = subject.collect {|feature| feature.properties['state_code'] }
+        expect(response_state_codes).to eq(state_codes)
+      end
+
+      context 'given users for all federal states of germany' do
+        let(:users) do
+          state_codes.each_with_index do |state_code, i|
+            create_list(:user, i, country_code: 'DE', state_code: state_code)
+          end
+        end
+        before { users }
+
+        it 'returns counts for all federal states' do
+          counts = subject.collect {|feature| feature.properties['user_count_total'] }
+          expect(counts).to eq (0..15).to_a
+        end
+
+        it 'returns normalized counts for all federal states' do
+          normalized_counts = subject.collect {|feature| feature.properties['user_count_normalized'] }
+          expect(normalized_counts).to all(be_between(0, 1))
+        end
+      end
+    end
+  end
+end
