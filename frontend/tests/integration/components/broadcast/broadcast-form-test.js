87e33fe3479f94482f79ea342d7f7575bce16056
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-09-14 10:46:21 +0200

    nest broadcast-form component

--- frontend/tests/integration/components/broadcast/broadcast-form-test.js
+++ frontend/tests/integration/components/broadcast/broadcast-form-test.js
@@ -4,8 +4,8 @@ import { setupComponentTest } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 
 let intl;
-describe('Integration | Component | broadcast form', function() {
-  setupComponentTest('broadcast-form', {
+describe('Integration | Component | broadcast/broadcast-form', function() {
+  setupComponentTest('broadcast/broadcast-form', {
     integration: true,
     setup() {
       intl = this.container.lookup('service:intl');
@@ -14,14 +14,14 @@ describe('Integration | Component | broadcast form', function() {
   });
 
   it('renders title and description', function() {
-    this.render(hbs`{{broadcast-form}}`);
+    this.render(hbs`{{broadcast/broadcast-form}}`);
     expect(this.$().text()).to.match(/Title/);
     expect(this.$().text()).to.match(/Description/);
 
     this.render(hbs`
-       {{#broadcast-form}}
+       {{#broadcast/broadcast-form}}
          template block text
-       {{/broadcast-form}}
+       {{/broadcast/broadcast-form}}
      `);
     expect(this.$().text()).to.match(/template block text/);
   });
