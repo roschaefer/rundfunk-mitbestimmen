d6909684c17b53e2791b5405dcca848e73ff9b30
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-09-20 11:21:16 +0200

    first version of medium spec

--- backend/app/models/statistic/medium.rb
+++ backend/app/models/statistic/medium.rb
@@ -1,2 +1,11 @@
-class Statistic::Medium < ApplicationRecord
+module Statistic
+  class Medium < ApplicationRecord
+    self.primary_key = :id
+    belongs_to :station, class_name: '::Station', foreign_key: :id
+    # this isn't strictly necessary, but it will prevent
+    # rails from calling save, which would fail anyway.
+    def readonly?
+      true
+    end
+  end
 end
--- backend/spec/models/statistic/medium_spec.rb
+++ backend/spec/models/statistic/medium_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Statistic::Medium, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:medium) { create(:medium) }
+  subject { described_class.find(medium.id) }
+  context 'medium without broadcasts' do
+    it { is_expected.to be_present }
+  end
+
+  context 'medium with a broadcast' do
+    context 'broadcast without impressions' do
+      let(:medium) { create(:medium, broadcasts: create_list(:broadcast, 1)) }
+      it { is_expected.to be_present }
+    end
+  end
 end
