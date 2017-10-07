bdb78b8f92db8702f49e33aae0cb99359facccf3
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-10-07 18:28:20 +0200

    style column plot in the center

--- frontend/app/controllers/index.js
+++ frontend/app/controllers/index.js
@@ -13,6 +13,23 @@ export default Ember.Controller.extend({
       title: {
         text: ''
       },
+      xAxis:{
+        categories: [
+          this.get('intl').t('index.goal.actual.series.events'),
+          this.get('intl').t('index.goal.actual.series.family'),
+          this.get('intl').t('index.goal.actual.series.series'),
+          this.get('intl').t('index.goal.actual.series.culture'),
+          this.get('intl').t('index.goal.actual.series.music'),
+          this.get('intl').t('index.goal.actual.series.politics'),
+          this.get('intl').t('index.goal.actual.series.presenation'),
+          this.get('intl').t('index.goal.actual.series.quiz'),
+          this.get('intl').t('index.goal.actual.series.religion'),
+          this.get('intl').t('index.goal.actual.series.show'),
+          this.get('intl').t('index.goal.actual.series.movie'),
+          this.get('intl').t('index.goal.actual.series.sport'),
+          this.get('intl').t('index.goal.actual.series.spot'),
+        ],
+      },
       yAxis: {
         title: {
           text: this.get('intl').t('index.goal.actual.yAxis'),
@@ -21,57 +38,17 @@ export default Ember.Controller.extend({
       tooltip: {
         pointFormat: '<b>{point.y:.3f} Mio. €</b>'
       },
-      legend: {
-        itemDistance: 10
-      },
-      plotOptions: {
-        pie: {
-          allowPointSelect: true,
-          cursor: 'pointer',
-          dataLabels: {
-            enabled: false
-          },
-          showInLegend: true,
-        }
-      },
     }
   }),
   actualChartData: Ember.computed('intl.locale', function() {
     return [{
-      innerSize: '50%',
-      colorByPoint: true,
-      name:  this.get('intl').t('index.goal.actual.name'),
-      data: [{
-        name:  this.get('intl').t('index.goal.actual.series.sport'),
-        y: 461.833
-      }, {
-        name:  this.get('intl').t('index.goal.actual.series.politics'),
-        y: 309.752,
-      }, {
-        name:  this.get('intl').t('index.goal.actual.series.television-play'),
-        y: 262.747
-      }, {
-        name:  this.get('intl').t('index.goal.actual.series.entertainment'),
-        y: 225.554
-      }, {
-        name:  this.get('intl').t('index.goal.actual.series.movie'),
-        y: 213.776
-      }, {
-        name:  this.get('intl').t('index.goal.actual.series.family'),
-        y: 114.992
-      }, {
-        name:  this.get('intl').t('index.goal.actual.series.culture'),
-        y: 50.995
-      }, {
-        name:  this.get('intl').t('index.goal.actual.series.religion'),
-        y: 10.074
-      }, {
-        name:  this.get('intl').t('index.goal.actual.series.spot'),
-        y: 3.648
-      }, {
-        name:  this.get('intl').t('index.goal.actual.series.music'),
-        y: 1.663
-      } ]
+      name:  this.get('intl').t('index.goal.actual.name.dasErste'),
+      color: '#1e5b9a',
+      data: [ 0, 114.9923, 262.7471, 50.9954, 1.6636, 309.7525, 0, 0, 10.0774, 225.5548, 213.7767, 461.8339, 3.6481]
+    },{
+      name:  this.get('intl').t('index.goal.actual.name.zdf'),
+      color: '#fa7d19',
+      data: [171.697, 36.490, 422.425, 86.055, 6.201, 172.130, 32.672, 32, 0, 160.639, 73.300, 368.039, 0]
     }];
   }),
 });
