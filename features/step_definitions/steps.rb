732a7aff3ff1f730c54d68f11faa816d5b38c37d
zmagoD <zmago_devetak@yahoo.com>
2017-10-13 21:53:09 +0200

    add picture_url case in cucumber spec close #42

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -886,6 +886,7 @@ end
 
 Then(/^I see only this broadcast and nothing else/) do
   expect(page).to have_css('.title', count: 1)
+  expect(page).to have_css('.bradcast-image-container', count: 1)
   Broadcast.where.not(title: 'Medienmagazin').find_each do |broadcast|
     expect(page).not_to have_text(broadcast.title)
   end
