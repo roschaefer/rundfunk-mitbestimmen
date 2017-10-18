8b0ba5993eb26f353865ec756d1fb2719e98385d
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-10-19 01:41:40 +0200

    acceptance-test is failing for the right reason! :simple_smile:

--- frontend/tests/acceptance/find-broadcasts/close-login-window-test.js
+++ frontend/tests/acceptance/find-broadcasts/close-login-window-test.js
@@ -2,16 +2,18 @@ import { describe, it, context, beforeEach, afterEach } from 'mocha';
 import { expect } from 'chai';
 import startApp from '../../helpers/start-app';
 import destroyApp from '../../helpers/destroy-app';
-import { mockSetup, mockTeardown, mockFindAll, mockQuery, buildList } from 'ember-data-factory-guy';
+import { mockSetup, mockTeardown, mockFindAll, mockQuery, mockCreate, buildList } from 'ember-data-factory-guy';
+import { authenticateSession } from 'frontend/tests/helpers/ember-simple-auth';
 
 describe('Acceptance | find broadcasts/close login window', function() {
-  let application, broadcasts, broadcastsMock;
+  let application, broadcasts, broadcastsMock, impressionUpdateMock;
   let message = 'To keep access to your data';
 
   beforeEach(function() {
     application = startApp();
     mockSetup();
     broadcastsMock = mockQuery('broadcast');
+    impressionUpdateMock = mockCreate('impression');
     mockFindAll('station');
     mockFindAll('medium');
   });
@@ -23,13 +25,13 @@ describe('Acceptance | find broadcasts/close login window', function() {
   });
 
 
-  context('unauthenticated user', function() {
-    context('given 12 broadcasts', function() {
-      beforeEach(function() {
-        broadcasts = buildList('broadcast', 12).add({ meta: { 'total-pages': 2, 'total-count': 12} });
-        broadcastsMock.returns({json: broadcasts});
-      });
+  context('given 12 broadcasts', function() {
+    beforeEach(function() {
+      broadcasts = buildList('broadcast', 12).add({ meta: { 'total-pages': 2, 'total-count': 12} });
+      broadcastsMock.returns({json: broadcasts});
+    });
 
+    context('unauthenticated user', function() {
       describe('visit /find-broadcasts', function(){
         it('does not show the login dialog', function() {
           visit('/find-broadcasts/');
@@ -44,7 +46,7 @@ describe('Acceptance | find broadcasts/close login window', function() {
             click('.decision-card-support-button');
             return andThen(() => {
               expect(find('.auth0-lock-widget-container').text().trim()).to.have.string(message);
-              // also no Error: The adapter operation was aborted
+              expect(impressionUpdateMock.timesCalled).to.equal(0);
             });
           })
         })
@@ -54,11 +56,43 @@ describe('Acceptance | find broadcasts/close login window', function() {
             visit('/find-broadcasts/');
             click('.find-broadcasts-navigation-next');
             return andThen(() => {
-              // no Error: The adapter operation was aborted
+              expect(impressionUpdateMock.timesCalled).to.equal(0);
             });
           })
         })
       })
     })
+
+    context('authenticated user', function() {
+      beforeEach(function() {
+        const sessionData = {idToken : 1};
+        authenticateSession(application, sessionData);
+      });
+
+      describe('visit /find-broadcast', function(){
+        it('sends a parameter ?mark_as_seen=true', function() {
+          broadcastsMock = mockQuery('broadcast', {
+            mark_as_seen: true
+          });
+          visit('/find-broadcasts/');
+          return andThen(() => {
+            expect(broadcastsMock.timesCalled).to.equal(1);
+          });
+        });
+      });
+
+      describe('click on support button', function(){
+        it('creates a positive impression', function() {
+          visit('/find-broadcasts/');
+          impressionUpdateMock = mockCreate('impression').match({
+            response: 'positive'
+          });
+          click('.decision-card-support-button');
+          return andThen(() => {
+            expect(impressionUpdateMock.timesCalled).to.equal(1);
+          });
+        })
+      })
+    });
   })
 });
