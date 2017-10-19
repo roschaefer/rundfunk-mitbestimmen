9e93c791e04b1822a19d7ef8d10f90ed4bc370f4
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-10-19 02:04:35 +0200

    integration test decision card when unauthenticated

--- frontend/tests/integration/components/decision-card-test.js
+++ frontend/tests/integration/components/decision-card-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { expect } from 'chai';
 import { context, beforeEach, describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
@@ -5,6 +6,9 @@ import hbs from 'htmlbars-inline-precompile';
 import { make, manualSetup } from 'ember-data-factory-guy';
 
 let broadcast, intl;
+const sessionStub = Ember.Service.extend({
+  isAuthenticated: true,
+});
 
 describe('Integration | Component | decision card', function() {
   setupComponentTest('decision-card', {
@@ -14,6 +18,7 @@ describe('Integration | Component | decision card', function() {
       intl.setLocale('en');
     }
   });
+
   beforeEach(function(){
     manualSetup(this.container);
     broadcast = make('broadcast', {
@@ -21,70 +26,97 @@ describe('Integration | Component | decision card', function() {
     });
   });
 
-  it('renders', function() {
-    this.set('broadcast', broadcast);
-    this.render(hbs`{{decision-card broadcast=broadcast}}`);
-    expect(this.$()).to.have.length(1);
-  });
-
-  it('displays the title', function() {
-    this.set('broadcast', broadcast);
-    this.render(hbs`{{decision-card broadcast=broadcast}}`);
-    expect(this.$('.title').text().trim()).to.eq('This is the title');
-  });
-
-  describe('click on support button', function() {
-    it('turns the heart red', function() {
-      this.set('broadcast', broadcast);
-      this.render(hbs`{{decision-card broadcast=broadcast}}`);
-      expect(this.$('.decision-card-support-button i.icon.heart').hasClass('red')).to.be.false;
-      this.$('.decision-card-support-button').click();
-      expect(this.$('.decision-card-support-button i.icon.heart').hasClass('red')).to.be.true;
-    });
-
-    it('saves the response on the broadcast', function() {
-      this.set('broadcast', broadcast);
-      this.render(hbs`{{decision-card broadcast=broadcast}}`);
-      expect(broadcast.get('response')).to.be.undefined;
-      this.$('.decision-card-support-button').click();
-      expect(broadcast.get('response')).to.eq('positive');
-    });
-
-    it('calls respondAction and returns the broadcast, if respondAction given', function(done) {
-      let broadcast = make('broadcast', {
-        title: 'this title is to be expected'
-      });
-      this.set('broadcast', broadcast);
-      this.set('callback', function(broadcast) {
-        expect(broadcast.get('title')).to.eq('this title is to be expected');
-        done();
-      });
-      this.render(hbs`{{decision-card broadcast=broadcast respondAction=callback}}`);
-      this.$('.decision-card-support-button').click();
-    });
-
-    context('broadcast is already supported', function() {
-      beforeEach(function(){
-        make('impression', {
-          broadcast: broadcast,
-          response: 'positive'
+  context('authenticated', function() {
+    describe('click on support button', function() {
+      it('calls the login action', function(done) {
+        this.set('broadcast', broadcast);
+        this.set('loginAction', function() {
+          done();
         });
+        this.render(hbs`{{decision-card broadcast=broadcast login=loginAction}}`);
+        this.$('.decision-card-support-button').click();
       });
 
-      it('turns the heart grey', function() {
+      it('keeps the response to the broadcast unchanged', function() {
         this.set('broadcast', broadcast);
         this.render(hbs`{{decision-card broadcast=broadcast}}`);
-        expect(this.$('.decision-card-support-button i.icon.heart').hasClass('red')).to.be.true;
         this.$('.decision-card-support-button').click();
+        expect(broadcast.get('response')).to.be.undefined;
+      });
+    });
+  });
+
+  context('authenticated', function() {
+    beforeEach(function(){
+      this.register('service:session', sessionStub);
+      this.inject.service('session', { as: 'sessionService' });
+    });
+
+    it('renders', function() {
+      this.set('broadcast', broadcast);
+      this.render(hbs`{{decision-card broadcast=broadcast}}`);
+      expect(this.$()).to.have.length(1);
+    });
+
+    it('displays the title', function() {
+      this.set('broadcast', broadcast);
+      this.render(hbs`{{decision-card broadcast=broadcast}}`);
+      expect(this.$('.title').text().trim()).to.eq('This is the title');
+    });
+
+    describe('click on support button', function() {
+      it('turns the heart red', function() {
+        this.set('broadcast', broadcast);
+        this.render(hbs`{{decision-card broadcast=broadcast}}`);
         expect(this.$('.decision-card-support-button i.icon.heart').hasClass('red')).to.be.false;
+        this.$('.decision-card-support-button').click();
+        expect(this.$('.decision-card-support-button i.icon.heart').hasClass('red')).to.be.true;
       });
 
-      it('toggles the response on the broadcast', function() {
+      it('saves the response on the broadcast', function() {
         this.set('broadcast', broadcast);
         this.render(hbs`{{decision-card broadcast=broadcast}}`);
-        expect(broadcast.get('response')).to.eq('positive');
+        expect(broadcast.get('response')).to.be.undefined;
         this.$('.decision-card-support-button').click();
-        expect(broadcast.get('response')).to.eq('neutral');
+        expect(broadcast.get('response')).to.eq('positive');
+      });
+
+      it('calls respondAction and returns the broadcast, if respondAction given', function(done) {
+        let broadcast = make('broadcast', {
+          title: 'this title is to be expected'
+        });
+        this.set('broadcast', broadcast);
+        this.set('callback', function(broadcast) {
+          expect(broadcast.get('title')).to.eq('this title is to be expected');
+          done();
+        });
+        this.render(hbs`{{decision-card broadcast=broadcast respondAction=callback}}`);
+        this.$('.decision-card-support-button').click();
+      });
+
+      context('broadcast is already supported', function() {
+        beforeEach(function(){
+          make('impression', {
+            broadcast: broadcast,
+            response: 'positive'
+          });
+        });
+
+        it('turns the heart grey', function() {
+          this.set('broadcast', broadcast);
+          this.render(hbs`{{decision-card broadcast=broadcast}}`);
+          expect(this.$('.decision-card-support-button i.icon.heart').hasClass('red')).to.be.true;
+          this.$('.decision-card-support-button').click();
+          expect(this.$('.decision-card-support-button i.icon.heart').hasClass('red')).to.be.false;
+        });
+
+        it('toggles the response on the broadcast', function() {
+          this.set('broadcast', broadcast);
+          this.render(hbs`{{decision-card broadcast=broadcast}}`);
+          expect(broadcast.get('response')).to.eq('positive');
+          this.$('.decision-card-support-button').click();
+          expect(broadcast.get('response')).to.eq('neutral');
+        });
       });
     });
   });
