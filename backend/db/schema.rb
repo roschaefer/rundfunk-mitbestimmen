8a9ba01e0d6547d2476561d3afdb2f1885001284
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-10-23 23:30:22 +0200

    fully reversible migration to setup materialized view

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171029000924) do
+ActiveRecord::Schema.define(version: 20171111141249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20171029000924) do
     t.index ["topic_id"], name: "index_broadcasts_on_topic_id"
   end
 
-  create_table "format_translations", force: :cascade do |t|
+  create_table "format_translations", id: :serial, force: :cascade do |t|
     t.integer "format_id", null: false
     t.string "locale", null: false
     t.datetime "created_at", null: false
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 20171029000924) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "medium_translations", force: :cascade do |t|
+  create_table "medium_translations", id: :serial, force: :cascade do |t|
     t.integer "medium_id", null: false
     t.string "locale", null: false
     t.datetime "created_at", null: false
@@ -101,7 +101,7 @@ ActiveRecord::Schema.define(version: 20171029000924) do
     t.index ["name"], name: "index_stations_on_name", unique: true
   end
 
-  create_table "topic_translations", force: :cascade do |t|
+  create_table "topic_translations", id: :serial, force: :cascade do |t|
     t.integer "topic_id", null: false
     t.string "locale", null: false
     t.datetime "created_at", null: false
@@ -119,8 +119,8 @@ ActiveRecord::Schema.define(version: 20171029000924) do
   create_table "users", id: :serial, force: :cascade do |t|
     t.string "encrypted_password", default: "", null: false
     t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer "role", default: 0
     t.string "auth0_uid"
     t.boolean "has_bad_email", default: false
@@ -178,7 +178,7 @@ ActiveRecord::Schema.define(version: 20171029000924) do
              FROM impressions) a ON (true));
   SQL
 
-  create_view "statistic_broadcasts",  sql_definition: <<-SQL
+  create_view "statistic_broadcasts", materialized: true,  sql_definition: <<-SQL
       SELECT broadcasts.id,
       broadcasts.title,
       count(impressions.id) AS impressions,
