957bc6b704d59aff0da6b966ec3231a3b31abc75
sigu <sigumagwa@gmail.com>
2017-10-27 18:29:44 +0300

    Add: error page incase of unsucussful request

--- frontend/app/routes/broadcast.js
+++ frontend/app/routes/broadcast.js
@@ -2,8 +2,12 @@ import Ember from 'ember';
 import ResetScrollPositionMixin from 'frontend/mixins/reset-scroll-position';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, ResetScrollPositionMixin, {
-  model(params) {
-    return this.get('store').findRecord('broadcast', params.broadcast_id);
+export default Ember.Route.extend(
+  AuthenticatedRouteMixin,
+  ResetScrollPositionMixin,
+  {
+    model(params) {
+      return this.get('store').findRecord('broadcast', params.broadcast_id);
+    }
   }
-});
+);
