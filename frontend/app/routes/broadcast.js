70fa751f158297ed39be35000b7b14013f40cbf3
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-10-28 15:29:33 +0200

    Transition to 404 on backend 404 HTTP response

--- frontend/app/routes/broadcast.js
+++ frontend/app/routes/broadcast.js
@@ -2,12 +2,18 @@ import Ember from 'ember';
 import ResetScrollPositionMixin from 'frontend/mixins/reset-scroll-position';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(
-  AuthenticatedRouteMixin,
-  ResetScrollPositionMixin,
-  {
-    model(params) {
-      return this.get('store').findRecord('broadcast', params.broadcast_id);
+export default Ember.Route.extend(AuthenticatedRouteMixin, ResetScrollPositionMixin, {
+  model(params) {
+    return this.get('store').findRecord('broadcast', params.broadcast_id);
+  },
+  actions: {
+    error(error) {
+      if (error.errors.isAny('status', '404')) {
+        this.transitionTo('/404'); // '404' gives a name error, why??
+      } else {
+        // Let the route above this handle the error.
+        return true;
+      }
     }
   }
-);
+});
