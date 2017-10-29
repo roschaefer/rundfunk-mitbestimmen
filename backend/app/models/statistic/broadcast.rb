6019e9188a7f1c4f349b538cd728a56d261af9f4
Domenico Angilletta <domenico.angilletta@gmail.com>
2017-10-29 18:52:54 +0100

    Replace SQL VIEW definition with Arel table construction
    This change removes the inline SQL VIEW definition for historical broadcast statistics, and replace it with an Arel generated sql.

--- backend/app/models/statistic/broadcast.rb
+++ backend/app/models/statistic/broadcast.rb
@@ -44,6 +44,13 @@ module Statistic
           global_average_per_impression
         ), 0]
       )
+
+    def self.kpis_at(time)
+      historical_kpis(time)
+    end
+
+    def self.kpis_for_broadcast_at(broadcast, time)
+      historical_kpis(time).where(Arel.sql("id = #{broadcast.id}"))
     end
 
     def self.global_average_per_impression
@@ -58,6 +65,79 @@ module Statistic
       )
     end
     private
+
+    def self.historical_kpis(time)
+      # 1 row for each broadcast
+      table = Arel::SelectManager.new(Arel.sql("(#{broadcast_statistics_table.to_sql}) as results"))
+      # Add average selection per impression column
+      table = table.join(average_amount_per_selection_table, Arel::Nodes::OuterJoin).on(Arel.sql("true"))
+      # Add with block with temporal filtered impressions
+      table = table.with(with_temporal_impressions(time))
+      # Project final kpi values to expose
+      table = table.project(
+        "id",
+        "title",
+        "impressions",
+        "(CAST(positives AS FLOAT)/CAST(NULLIF(impressions,0)   AS FLOAT))         AS approval",
+        "COALESCE((CAST(total     AS FLOAT)/CAST(NULLIF(positives,0) AS FLOAT)),0) AS average",
+        "total",
+        "impressions * average_amount_per_selection as expected_amount"
+      )
+      # Add table entries for broadcasts without impressions
+      table = table.union(default_values_for_broadcasts_without_impressions)
+
+      # Expose SelectManager object
+      final = Arel::SelectManager.new(Arel.sql("#{table.to_sql} as kpis"))
+      final.project(Arel.star)
+    end
+
+    def self.default_values_for_broadcasts_without_impressions
+      impressions = Arel::Table.new(:impressions)
+      broadcasts_without_impressions = Arel::Table.new(:broadcasts)
+
+      broadcasts_without_impressions.
+          join(impressions, Arel::Nodes::OuterJoin).on(broadcasts_without_impressions[:id].eq(impressions[:broadcast_id])).
+          where(impressions[:broadcast_id].eq(nil)).
+          project(
+              broadcasts_without_impressions[:id].as("id"),
+              broadcasts_without_impressions[:title].as("title"),
+              "0 AS impressions",
+              "NULL AS approval",
+              "NULL AS average",
+              "0 AS total",
+              "0 AS expected_amount"
+          )
+    end
+
+    def self.broadcast_statistics_table
+      broadcasts = Arel::Table.new(:broadcasts)
+      impressions = Arel::Table.new(:impressions)
+
+      broadcasts.
+        join(impressions).on(broadcasts[:id].eq(impressions[:broadcast_id])).
+        group(impressions[:broadcast_id], broadcasts[:title]).
+        project(
+            impressions[:broadcast_id].as("id"), 
+            broadcasts[:title].as("title"),
+            impressions[:id].count.as("impressions"),
+            "COALESCE(SUM(CASE WHEN impressions.response = 1 THEN 1 ELSE 0 END), 0) AS positives",
+            "COALESCE(SUM(amount),0) AS total"
+        )
+    end
+
+    def self.average_amount_per_selection_table
+      all_impressions = Arel::Table.new(:impressions)
+
+      all_impressions.
+        project("SUM(amount) / COUNT(*) AS average_amount_per_selection").
+        as("general")
+    end
+
+    def self.with_temporal_impressions(time)
+      temporal_impressions = Arel::Table.new(:impressions)
+
+      Arel::Nodes::As.new(temporal_impressions, Arel.sql("(#{Impression.as_of(time).to_sql})"))
+    end
 
     # this isn't strictly necessary, but it will prevent
     # rails from calling save, which would fail anyway.
