28d4be217a0cc7c2256dd34fa865094629f13953
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-10-29 02:20:09 +0200

    View Definition of statistic_broadcasts with Arel
    Generate the view definition of statistic_broadcast with arel. The
    resulting query is much more optimized, has a more accurate average and
    is easier to parameterize.

--- backend/app/models/statistic/broadcast.rb
+++ backend/app/models/statistic/broadcast.rb
@@ -5,6 +5,56 @@ module Statistic
 
     belongs_to :broadcast, class_name: '::Broadcast', foreign_key: :id
 
+    # Single Source of Truth
+    # Use this view definition to generate the stored SQL
+    def self.view_definition
+      broadcasts = Arel::Table.new(:broadcasts)
+      impressions = Arel::Table.new(:impressions)
+      broadcasts.project(
+        broadcasts[:id].as('id'),
+        broadcasts[:title].as('title'),
+        impressions[:id].count.as('impressions'),
+        impressions[:response].average.as('approval'),
+        impressions[:amount].average.as('average'),
+        self.total_amount(impressions).as('total'),
+        self.expected_amount(impressions).as('expected_amount')
+      ).join(impressions, Arel::Nodes::OuterJoin)
+        .on(impressions[:broadcast_id].eq(broadcasts[:id]))
+        .group(broadcasts[:id], broadcasts[:title])
+    end
+
+    def self.total_amount(impressions_table)
+      Arel::Nodes::NamedFunction.new(
+        'coalesce',
+        [impressions_table[:amount].sum,0]
+      )
+    end
+
+    def self.expected_amount(impressions_table)
+      Arel::Nodes::NamedFunction.new(
+        'coalesce',
+        [Arel::Nodes::Multiplication.new(
+          impressions_table[:id].count,
+          self.global_average_per_impression
+        ), 0]
+      )
+    end
+
+    def self.global_average_per_impression
+      impressions = Arel::Table.new(:impressions)
+      Arel::Table.new(:impressions).project(
+        Arel::Nodes::NamedFunction.new(
+          'avg',
+          [Arel::Nodes::NamedFunction.new(
+            'coalesce',
+            [impressions[:amount], 0]
+          )]
+        )
+      )
+    end
+
+
+
     private
 
     # this isn't strictly necessary, but it will prevent
