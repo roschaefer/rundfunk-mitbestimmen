0adcde992b9c660f5ef11cbfa9a38de42e913800
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-11-01 23:09:25 +0100

    DRY up navigation component
    Reuse code in find-broadcasts/navigation/distribute-button component

--- frontend/app/components/find-broadcasts/navigation/distribute-budget.js
+++ frontend/app/components/find-broadcasts/navigation/distribute-budget.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: '',
+
+  distibuteButtonCss: Ember.computed('recentPositives', function() {
+    switch(this.get('recentPositives')) {
+      case 0: return ' disabled ';
+      case 1: return ' ';
+      case 2: return ' ';
+      default: return ' primary ';
+    }
+  }),
 });
