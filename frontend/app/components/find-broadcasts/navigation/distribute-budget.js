2160556b110b2bab5a05dd28341f25696065b2c1
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-11-13 18:13:06 +0100

    Increase size of distribute button gradually
    Hopefully it gets more attention

--- frontend/app/components/find-broadcasts/navigation/distribute-budget.js
+++ frontend/app/components/find-broadcasts/navigation/distribute-budget.js
@@ -5,10 +5,10 @@ export default Ember.Component.extend({
 
   buttonCss: Ember.computed('recentPositives', function() {
     switch(this.get('recentPositives')) {
-      case 0: return ' disabled ';
-      case 1: return ' ';
-      case 2: return ' ';
-      default: return ' primary ';
+      case 0: return 'disabled';
+      case 1: return 'large';
+      case 2: return 'big';
+      default: return 'big primary';
     }
   }),
 });
