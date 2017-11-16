cfc5ce7b453dd809ffed71774df367222d738629
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-11-16 16:02:27 +0100

    Set sentry dsn only when ENV variable set

--- backend/config/initializers/sentry.rb
+++ backend/config/initializers/sentry.rb
@@ -1,4 +1,7 @@
-Raven.configure do |config|
-  config.dsn = 'https://05936732e280480982910ff0c67aa72d:4839343d52ed46c8affc57f621206611@sentry.io/244931'
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+dsn = ENV['SENTRY_DSN']
+if dsn
+  Raven.configure do |config|
+    config.dsn = dsn
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  end
 end
