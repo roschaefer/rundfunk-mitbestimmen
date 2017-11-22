c7c8171ac8ade14eccc1e4fd2791726e79b138d8
Robert Schaefer <robert.schaefer@student.hpi.de>
2017-11-22 01:15:00 +0100

    Acesss frontend broadcast/:id unauthenticated

--- frontend/tests/integration/components/broadcast/broadcast-details-test.js
+++ frontend/tests/integration/components/broadcast/broadcast-details-test.js
@@ -1,9 +1,15 @@
+import Ember from 'ember';
 import { expect } from 'chai';
-import { describe, it } from 'mocha';
+import { context, beforeEach, describe, it } from 'mocha';
 import { setupComponentTest } from 'ember-mocha';
+import { make, manualSetup } from 'ember-data-factory-guy';
 import hbs from 'htmlbars-inline-precompile';
 
-let intl;
+let broadcast, intl;
+const sessionStub = Ember.Service.extend({
+  isAuthenticated: true,
+});
+
 describe('Integration | Component | broadcast/broadcast details', function() {
   setupComponentTest('broadcast/broadcast-details', {
     integration: true,
@@ -13,8 +19,56 @@ describe('Integration | Component | broadcast/broadcast details', function() {
     }
   });
 
+  beforeEach(function(){
+    manualSetup(this.container);
+    broadcast = make('broadcast', {
+      title: 'This is the title'
+    });
+  });
+
   it('renders', function() {
-    this.render(hbs`{{broadcast/broadcast-details}}`);
+    this.render(hbs`{{broadcast/broadcast-details broadcast=broadcast}}`);
     expect(this.$()).to.have.length(1);
+  });
+
+  context('unauthenticated', function() {
+    describe('click on edit button', function() {
+      it('asks to login', function(done) {
+        this.set('broadcast', broadcast);
+        this.set('loginAction', function() {
+          done();
+        });
+        this.render(hbs`{{broadcast/broadcast-details broadcast=broadcast loginAction=loginAction}}`);
+        this.$('.edit.button').click();
+      });
+    });
+
+    describe('click on support button', function() {
+      it('asks to login', function(done){
+        this.set('broadcast', broadcast);
+        this.set('loginAction', function() {
+          done();
+        });
+        this.render(hbs`{{broadcast/broadcast-details broadcast=broadcast loginAction=loginAction}}`);
+        this.$('.decision-card-support-button').click();
+      });
+    });
+  });
+
+  context('authenticated', function() {
+    beforeEach(function(){
+      this.register('service:session', sessionStub);
+      this.inject.service('session', { as: 'sessionService' });
+    });
+
+    describe('click on support button', function() {
+      it('calls respondAction', function() {
+        this.set('broadcast', broadcast);
+        expect(broadcast.get('response')).to.be.undefined;
+        this.render(hbs`{{broadcast/broadcast-details broadcast=broadcast}}`);
+        this.$('.decision-card-support-button').click();
+        expect(broadcast.get('response')).to.eq('positive');
+      });
+    });
   });
 });
