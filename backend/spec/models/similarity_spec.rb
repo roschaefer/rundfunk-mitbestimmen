1854103a096e88d7e19ab2a6d77445d60db7c628
Domenico Angilletta <domenico.angilletta@gmail.com>
2018-02-15 16:51:14 +0100

    Remove Rubocop offenses

--- backend/spec/models/similarity_spec.rb
+++ backend/spec/models/similarity_spec.rb
@@ -11,32 +11,32 @@ RSpec.describe Similarity, type: :model do
       create(:impression, id: 2, broadcast: Broadcast.last, user: User.first, response: 1)
     end
 
-    it "calls delete_all" do
+    it 'calls delete_all' do
       expect(Similarity).to receive(:destroy_all)
       Similarity.compute_all
     end
 
-    it "saves one similarity pair" do
-      expect{subject}.to change{Similarity.count}.from(0).to(1)
+    it 'saves one similarity pair' do
+      expect { subject }.to change { Similarity.count }.from(0).to(1)
     end
   end
 
   describe 'compute' do
     subject { Similarity.compute(broadcast1, broadcast2) }
-    let(:broadcast1) {build(:broadcast)}
-    let(:broadcast2) {build(:broadcast)}
-    let(:user1) {build(:user)}
-    let(:user2) {build(:user)}
+    let(:broadcast1) { build(:broadcast) }
+    let(:broadcast2) { build(:broadcast) }
+    let(:user1) { build(:user) }
+    let(:user2) { build(:user) }
 
     describe 'no common users with positive impressions (supporters)' do
       before do
         create(:impression, id: 1, broadcast: broadcast1, user: user1, response: 1)
         create(:impression, id: 2, broadcast: broadcast2, user: user2, response: 1)
-        create(:impression, id: 3, broadcast: broadcast2, user: user1, response: :neutral) #ignored
+        create(:impression, id: 3, broadcast: broadcast2, user: user1, response: :neutral) # ignored
       end
 
-      it "has value 0" do
-          expect(subject.value).to eq(0)
+      it 'has value 0' do
+        expect(subject.value).to eq(0)
       end
     end
 
@@ -46,15 +46,15 @@ RSpec.describe Similarity, type: :model do
         create(:impression, id: 2, broadcast: broadcast2, user: user1, response: 1)
       end
 
-      it "has value 1" do
+      it 'has value 1' do
         expect(subject.value).to eq(1)
       end
     end
 
     describe 'with half supporters in common' do
-      let(:user3) {build(:user)}
-      let(:user4) {build(:user)}
-      let(:user5) {build(:user)}
+      let(:user3) { build(:user) }
+      let(:user4) { build(:user) }
+      let(:user5) { build(:user) }
 
       before do
         create(:impression, id: 1, broadcast: broadcast1, user: user1, response: 1)
@@ -65,7 +65,7 @@ RSpec.describe Similarity, type: :model do
         create(:impression, id: 6, broadcast: broadcast2, user: user5, response: :neutral) # ignored
       end
 
-      it "has value 0.5" do
+      it 'has value 0.5' do
         expect(subject.value).to eq(0.25) # 1 / 4
       end
     end
