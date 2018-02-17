cf7d9a463cded6269dbd2750c72076053a19920d
Robert Schaefer <robert.schaefer@student.hpi.de>
2018-02-17 19:37:00 +0100

    Massive performance teak (1.2 seconds)

--- backend/app/models/similarity.rb
+++ backend/app/models/similarity.rb
@@ -2,32 +2,40 @@ class Similarity < ApplicationRecord
   belongs_to :broadcast1, foreign_key: 'broadcast1_id', class_name: 'Broadcast'
   belongs_to :broadcast2, foreign_key: 'broadcast2_id', class_name: 'Broadcast'
 
-  def self.compute_all
-    Similarity.destroy_all
-    Broadcast.find_each do |broadcast1|
-      Broadcast.where('id > ?', broadcast1.id).each do |broadcast2|
-        similarity = compute(broadcast1, broadcast2)
-        similarity.save if similarity.value.positive?
+  def self.compute_all(threshold = 0)
+    Similarity.transaction do
+      Similarity.delete_all
+
+      supporters_per_broadcast = {}
+      Broadcast.find_each do |broadcast|
+        supporters_per_broadcast[broadcast.id] = broadcast.impressions.positive.pluck(:user_id)
+      end
+
+      similarities = []
+      broadcast_ids = supporters_per_broadcast.keys
+
+      time = Time.now.to_s(:db)
+      broadcast_ids.each do |id1|
+        broadcast_ids.each do |id2|
+          if id2 > id1
+            ji = jaccard_index(supporters_per_broadcast[id1], supporters_per_broadcast[id2])
+            similarities << "(#{id1}, #{id2}, #{ji}, '#{time}', '#{time}')" if ji >= threshold
+          end
+        end
+      end
+
+      if similarities.present?
+        sql = "INSERT INTO similarities (broadcast1_id, broadcast2_id, value, created_at, updated_at) VALUES #{similarities.join(', ')}"
+        ActiveRecord::Base.connection.execute(sql)
       end
     end
   end
 
-  def self.compute(broadcast1, broadcast2)
-    similarity = Similarity.new(broadcast1: broadcast1, broadcast2: broadcast2)
-    similarity.compute_jaccard
-    similarity
-  end
-
-  def compute_jaccard
-    supporters1 = broadcast1.impressions.positive.pluck(:user_id)
-    supporters2 = broadcast2.impressions.positive.pluck(:user_id)
-
+  def self.jaccard_index(supporters1, supporters2)
     union_size = (supporters1 | supporters2).size
-    if union_size.zero?
-      self.value = 0
-    else
-      intersection_size = (supporters1 & supporters2).size
-      self.value = (intersection_size / union_size.to_f)
-    end
+    return 0 if union_size.zero?
+
+    intersection_size = (supporters1 & supporters2).size
+    intersection_size / union_size.to_f
   end
 end
