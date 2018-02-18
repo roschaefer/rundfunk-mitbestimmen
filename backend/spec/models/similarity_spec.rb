8cdd14686cc8c6f31115ff2d60d1a4370643f958
Robert Schaefer <robert.schaefer@student.hpi.de>
2018-02-19 00:22:30 +0100

    Implement mininum supporters constraint
    in order to avoid cold start inaccuracy of jaccard similarity

--- backend/spec/models/similarity_spec.rb
+++ backend/spec/models/similarity_spec.rb
@@ -2,18 +2,43 @@ require 'rails_helper'
 
 RSpec.describe Similarity, type: :model do
   describe '::compute_all' do
+    subject { setup && Similarity.compute_all(threshold: threshold, minimum_supporters: minimum_supporters) }
+    let(:minimum_supporters) { 0 }
+
+    it 'calls delete_all' do
+      expect(Similarity).to receive(:delete_all)
+      Similarity.compute_all
+    end
+
+    describe 'minimum supporters == 2' do
+      let(:threshold) { 0 }
+      let(:minimum_supporters) { 2 }
+      let(:setup) { create_list(:broadcast, 2) }
+
+      it 'surpresses cold start similarities' do
+        expect { subject }.not_to(change { Similarity.count })
+      end
+
+      context '2 broadcasts with 2 supporters each' do
+        let(:setup) do
+          super()
+          create_list(:impression, 2, broadcast: Broadcast.first, response: :positive)
+          create_list(:impression, 2, broadcast: Broadcast.last, response: :positive)
+        end
+
+        it 'saves one similarity pair' do
+          expect { subject }.to change { Similarity.count }.from(0).to(1)
+        end
+      end
+    end
+
     describe 'threshold == 0.5' do
-      subject { setup && Similarity.compute_all(0.5) }
+      let(:threshold) { 0.5 }
       let(:setup) do
         create_list(:broadcast, 3)
         create_list(:user, 2)
         create(:impression, id: 1, broadcast: Broadcast.first, user: User.first, response: :positive)
         create(:impression, id: 2, broadcast: Broadcast.last, user: User.first, response: :positive)
-      end
-
-      it 'calls delete_all' do
-        expect(Similarity).to receive(:delete_all)
-        Similarity.compute_all
       end
 
       it 'saves one similarity pair' do
@@ -32,8 +57,8 @@ RSpec.describe Similarity, type: :model do
       describe 'the value of the first created similarity' do
         context 'no common users with positive impressions (supporters)' do
           let(:setup) do
-            create(:impression, id: 1, broadcast: broadcast1, user: user1, response: 1)
-            create(:impression, id: 2, broadcast: broadcast2, user: user2, response: 1)
+            create(:impression, id: 1, broadcast: broadcast1, user: user1, response: :positive)
+            create(:impression, id: 2, broadcast: broadcast2, user: user2, response: :positive)
             create(:impression, id: 3, broadcast: broadcast2, user: user1, response: :neutral) # ignored
           end
 
@@ -44,8 +69,8 @@ RSpec.describe Similarity, type: :model do
 
         context 'with exactly the same supporters' do
           let(:setup) do
-            create(:impression, id: 1, broadcast: broadcast1, user: user1, response: 1)
-            create(:impression, id: 2, broadcast: broadcast2, user: user1, response: 1)
+            create(:impression, id: 1, broadcast: broadcast1, user: user1, response: :positive)
+            create(:impression, id: 2, broadcast: broadcast2, user: user1, response: :positive)
           end
 
           it '== 1' do
@@ -59,11 +84,11 @@ RSpec.describe Similarity, type: :model do
           let(:user5) { build(:user) }
 
           let(:setup) do
-            create(:impression, id: 1, broadcast: broadcast1, user: user1, response: 1)
-            create(:impression, id: 2, broadcast: broadcast1, user: user2, response: 1)
-            create(:impression, id: 3, broadcast: broadcast2, user: user1, response: 1)
-            create(:impression, id: 4, broadcast: broadcast2, user: user3, response: 1)
-            create(:impression, id: 5, broadcast: broadcast2, user: user4, response: 1) # ignored
+            create(:impression, id: 1, broadcast: broadcast1, user: user1, response: :positive)
+            create(:impression, id: 2, broadcast: broadcast1, user: user2, response: :positive)
+            create(:impression, id: 3, broadcast: broadcast2, user: user1, response: :positive)
+            create(:impression, id: 4, broadcast: broadcast2, user: user3, response: :positive)
+            create(:impression, id: 5, broadcast: broadcast2, user: user4, response: :positive)
             create(:impression, id: 6, broadcast: broadcast2, user: user5, response: :neutral) # ignored
           end
 
