3d8187d078641a7c09e3528bddf7987318108d67
Robert Schaefer <robert.schaefer@student.hpi.de>
2018-02-19 15:26:17 +0100

    Create route to send similarities as nodes & edges

--- backend/app/controllers/chart_data_controller.rb
+++ backend/app/controllers/chart_data_controller.rb
@@ -1,6 +1,19 @@
 require 'rgeo/geo_json'
 class ChartDataController < ApplicationController
-  skip_authorization_check only: :geojson
+  skip_authorization_check only: [:geojson, :similarities]
+
+
+  def similarities
+    broadcasts = []
+    edges = []
+    Similarity.find_each do |s|
+      broadcasts << s.broadcast1
+      broadcasts << s.broadcast2
+      edges << {source: s.broadcast1.title, target: s.broadcast2.title, value: s.value}
+    end
+    nodes = broadcasts.uniq.map {|broadcast| {id: broadcast.title, group: broadcast.medium.id}}
+    render json: {nodes: nodes, links: edges}
+  end
 
   def geojson
     template_feature_collection = RGeo::GeoJSON.decode(File.read(File.join(Rails.root, 'public', 'bundeslaender.geojson')), json_parser: :json)
