591d60f96fc4274a2a66fdeaab5c3f9c55c49e92
Robert Schaefer <robert.schaefer@student.hpi.de>
2018-02-20 19:51:32 +0100

    Refactoring: Get graph data in route

--- frontend/app/routes/visualize/graph.js
+++ frontend/app/routes/visualize/graph.js
@@ -2,4 +2,11 @@ import Route from '@ember/routing/route';
 import ResetScrollPositionMixin from 'frontend/mixins/reset-scroll-position';
 
 export default Route.extend(ResetScrollPositionMixin, {
-});
+  model() {
+    const host = this.store.adapterFor('summarized-statistic').get('host');
+    return fetch(`${host}/chart_data/similarities.json`).then(function(response) {
+        return response.json();
+      });
+    }
+  }
+);
