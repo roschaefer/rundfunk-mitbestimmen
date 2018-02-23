8c9b17c0dffe9c766ea6bcac482158aa794d4932
Robert Schaefer <robert.schaefer@student.hpi.de>
2018-02-24 00:07:48 +0100

    Test and implement dynamic ::minimum_supporters

--- backend/spec/workers/compute_similarities_worker_spec.rb
+++ backend/spec/workers/compute_similarities_worker_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
+
 RSpec.describe ComputeSimilaritiesWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '::minimum_supporters' do
+    subject { described_class.minimum_supporters }
+    context 'given 3 impressions for same broadcast' do
+      before { create_list(:impression, 100, broadcast: create(:broadcast)) }
+      it { is_expected.to eq 5 }
+    end
+
+    context 'given 3 impressions for different broadcast' do
+      before { create_list(:impression, 3) }
+      it { is_expected.to eq 0 }
+    end
+
+    context 'given no impressions' do
+      it { is_expected.to eq 0 }
+    end
+  end
 end
