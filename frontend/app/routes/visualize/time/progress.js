e9c182f37f3c4688ba53f0b115330ddc6bd0a76a
Robert Schaefer <robert.schaefer@student.hpi.de>
2018-03-07 23:09:51 +0100

    Fix translation issues and show broadcast title
    Broadcast title is visible in /visualize/time/progress/:broadcast_id

--- frontend/app/routes/visualize/time/progress.js
+++ frontend/app/routes/visualize/time/progress.js
@@ -2,4 +2,17 @@ import Route from '@ember/routing/route';
 import ResetScrollPositionMixin from 'frontend/mixins/reset-scroll-position';
 
 export default Route.extend(ResetScrollPositionMixin, {
+  model(params){
+    return this.store.findRecord('broadcast', params.broadcast_id);
+  },
+  actions: {
+    error(error) {
+      if (error.errors.isAny('status', '404')) {
+        this.transitionTo('/404'); // '404' gives a name error, why??
+      } else {
+        // Let the route above this handle the error.
+        return true;
+      }
+    }
+  }
 });
