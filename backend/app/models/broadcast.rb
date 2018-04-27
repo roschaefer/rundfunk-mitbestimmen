7411c6611b58bba3cc0d5a85950a4e4ac3aa5bce
Domenico Angilletta <domenico.angilletta@gmail.com>
2018-04-27 12:04:57 +0200

    Add Broadcast#to_graph_node instance method
    returns a hash with id, title, and group (medium id) used to render the similarity graph

--- backend/app/models/broadcast.rb
+++ backend/app/models/broadcast.rb
@@ -88,6 +88,14 @@ class Broadcast < ApplicationRecord
     end
   end
 
+  def to_graph_node
+    {
+      id: id,
+      title: title,
+      group: medium.id
+    }
+  end
+
   private
 
   def description_should_not_contain_urls
