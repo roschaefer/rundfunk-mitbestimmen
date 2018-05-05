630e85a15aef469ae78bd7a55e1d6af5b77b077b
Robert Schaefer <git@roschaefer.de>
2018-05-05 19:04:23 +0200

    Update all ruby dependencies

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -48,7 +48,9 @@ When(/^I visit the landing page$/) do
 end
 
 Then(/^I can read:$/) do |string|
-  expect(page).to have_text string
+  actual = page.text.gsub("\n", ' ')
+  expected = string.gsub("\n", ' ')
+  expect(actual).to include(expected)
 end
 
 When(/^(?:then |when )?I click on "([^"]*)"/) do |string|
@@ -669,7 +671,8 @@ end
 
 Then(/^the stations are ordered like this:$/) do |table|
   within('.filter-stations-field') do
-    expect(all('.item:not(.blank)').map(&:text)).to eq(table.rows.flatten)
+    items = all('.item:not(.blank)').map{|item| item.text.gsub("\n", ' ')}
+    expect(items).to eq(table.rows.flatten)
   end
 end
 
@@ -834,7 +837,7 @@ end
 
 Then(/^the drop down menu has excactly these items:$/) do |table|
   find('.selection', text: 'Filter by station').click
-  labels = all('.dropdown .item:not(.blank)').map(&:text)
+  labels = all('.dropdown .item:not(.blank)').map{|item| item.text.gsub("\n", ' ')}
   table.hashes.each_with_index do |row, i|
     expect(labels[i]).to eq row['Label']
   end
