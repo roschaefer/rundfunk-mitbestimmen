8f96e2b8c5e8cadb3584a79172a03dde39ddad17
Domenico Angilletta <domenico.angilletta@gmail.com>
2018-05-06 10:52:43 +0200

    Add Personalized Broadcast Similarities Filter Button
    The button allows to toggle between the complete broadcasts similarity graph and a graph that contains only broadcasts the user is supporting and their similarities.
    If a user is not authenticated, then the button is disabled.

---
+++ frontend/app/controllers/visualize/graph.js
@@ -0,0 +1,7 @@
+//app/controllers/application.js
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+
+export default Controller.extend({
+  session: service('session')
+});
