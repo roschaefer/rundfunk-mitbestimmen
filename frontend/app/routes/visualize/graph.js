4f35c5c7ca317a8772b2216a36cc27c7c8706a47
Domenico Angilletta <domenico.angilletta@gmail.com>
2018-05-06 10:52:43 +0200

    Add Personalized Broadcast Similarities Filter Button
    The button allows to toggle between the complete broadcasts similarity graph and a graph that contains only broadcasts the user is supporting and their similarities.
    If a user is not authenticated, then the button is disabled.

--- frontend/app/routes/visualize/graph.js
+++ frontend/app/routes/visualize/graph.js
@@ -3,11 +3,31 @@ import Route from '@ember/routing/route';
 import ResetScrollPositionMixin from 'frontend/mixins/reset-scroll-position';
 
 export default Route.extend(ResetScrollPositionMixin, {
-  model() {
-    const host = this.store.adapterFor('summarized-statistic').get('host');
-    return fetch(`${host}/chart_data/similarities.json`).then(function(response) {
+    model(params) {
+      const host = this.store.adapterFor('summarized-statistic').get('host');
+      let fullUrl = `${host}/chart_data/similarities.json`;
+
+      params.userspecific = params.userspecific == 'true'
+
+      if(params.userspecific){
+        fullUrl += `?userspecific=true`;
+      }
+
+      return fetch(fullUrl).then(function(response) {
         return response.json();
       });
+    },
+    queryParams: {
+      userspecific: {
+        refreshModel: true,
+        replace: true
+      }
+    },
+    actions: {
+      toggleUserSpecific(currentValue){
+        const switchedValue = currentValue != "true";
+        this.transitionTo({ queryParams: { userspecific: switchedValue }});
+      }
     }
   }
 );
