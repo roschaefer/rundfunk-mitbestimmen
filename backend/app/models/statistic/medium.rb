a3dc0e3773c0aad28e5e66953a285c016c4df384
Robert Schaefer <git@roschaefer.de>
2018-05-07 01:34:31 +0200

    Fix copy/paste bug: association of statistic model

--- backend/app/models/statistic/medium.rb
+++ backend/app/models/statistic/medium.rb
@@ -5,7 +5,7 @@ module Statistic
     attribute :name
 
     self.primary_key = :id
-    belongs_to :station, class_name: '::Station', foreign_key: :id
+    belongs_to :medium, class_name: '::Medium', foreign_key: :id
     # this isn't strictly necessary, but it will prevent
     # rails from calling save, which would fail anyway.
     def readonly?
