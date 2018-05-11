9031a9b1693c1c501ac8a35cd6004073d24494c7
Robert Schaefer <git@roschaefer.de>
2018-05-11 16:23:58 +0200

    Send authorization tokens in `fetch`

--- frontend/app/routes/visualize/graph.js
+++ frontend/app/routes/visualize/graph.js
@@ -15,11 +15,13 @@ export default Route.extend(ResetScrollPositionMixin, {
     model(params) {
       const host = this.store.adapterFor('summarized-statistic').get('host');
       let url = new URL(`${host}/chart_data/similarities.json`);
-      if (params.specificToUser){
+      let headers = {};
+      if (this.get('session.isAuthenticated') && params.specificToUser){
         url.searchParams.append('specific_to_user', params.specificToUser);
+        headers['Authorization'] = `Bearer ${this.get('session.data.authenticated.idToken')}`;
       }
 
-      return fetch(url).then(function(response) {
+      return fetch(url, {headers: headers}).then(function(response) {
         return response.json();
       });
     },
