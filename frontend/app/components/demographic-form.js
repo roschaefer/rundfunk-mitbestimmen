df5caf7494d1364bc7deaaf286a0c0ccc7c38473
Matt Rider <mattwr18@gmail.com>
2018-06-18 14:55:33 -0300

    Extract gender, ageGroups into service demography, passing tests

--- frontend/app/components/demographic-form.js
+++ frontend/app/components/demographic-form.js
@@ -1,17 +1,16 @@
 import Component from '@ember/component';
 import { isNone } from '@ember/utils';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
   tagName: '',
+  demography: service(),
   init() {
     this._super(...arguments);
-    this.set('genders', ['male', 'female', 'other']);
-    this.set('ageGroups', ['0-4', '5-9', '10-14', '15-19',
-      '20-24', '25-29', '30-34', '35-39', '40-44',
-      '45-49', '50-54', '55-59', '60-64', '65-69',
-      '70-74', '75-79', '80-84', '85-89', '90-94',
-      '95-99', '100+']);
+    this.set('genders', this.get('demography').get('genders'));
+    this.set('ageGroups', this.get('demography').get('ageGroups'));
   },
+
   actions: {
     update(attribute, value) {
       let user = this.get('user');
