94128eb02e8f9247f88f1ccde91192e12fe3e0b9
Robert Schäfer <git@roschaefer.de>
2018-08-01 00:47:58 +0200

    Fix `unknown error: DevToolsActivePort file`
    see: https://github.com/timgrossmann/InstaPy/issues/2362#issuecomment-402442392

--- features/support/env.rb
+++ features/support/env.rb
@@ -26,7 +26,7 @@ end
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
     chromeOptions: {
-      args: %w[headless disable-gpu window-size=1024,768]
+      args: %w[headless disable-gpu no-sandbox window-size=1024,768]
     }
   )
   Capybara::Selenium::Driver.new(
