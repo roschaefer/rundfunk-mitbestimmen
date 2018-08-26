6512ad83b7e3edcf38cbbfd596a1011a323d944a
Robert Schäfer <git@roschaefer.de>
2018-08-26 23:02:49 +0200

    Just don't authenticate the user on the server

--- frontend/app/routes/authentication/callback.js
+++ frontend/app/routes/authentication/callback.js
@@ -6,20 +6,22 @@ export default Route.extend({
   intl: service(),
   fastboot: service(),
   model() {
-    return this.get('session').authenticate('authenticator:auth0').then((data) => {
-      if (!this.get('fastboot.isFastBoot') && window._paq){
-        window._paq.push(['trackGoal', 4]);
-        //goalId 4 is "Signup/Login successful"
-      }
-
-      return this.store.queryRecord('user', {reload: true}).then((user) => {
-        const locale = user.get('locale');
-        if (locale) {
-          this.get('intl').setLocale(locale);
+    if (!this.get('fastboot.isFastBoot')){
+      return this.get('session').authenticate('authenticator:auth0').then(() => {
+        if (window._paq){
+          window._paq.push(['trackGoal', 4]);
+          //goalId 4 is "Signup/Login successful"
         }
-        const state = this.get('session.data.afterLoginRoute') || '/';
-        return this.transitionTo(state);
+
+        return this.store.queryRecord('user', {reload: true}).then((user) => {
+          const locale = user.get('locale');
+          if (locale) {
+            this.get('intl').setLocale(locale);
+          }
+          const state = this.get('session.data.afterLoginRoute') || '/';
+          return this.transitionTo(state);
+        });
       });
-    });
+    }
   }
 });
