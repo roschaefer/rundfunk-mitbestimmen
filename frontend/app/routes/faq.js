f08a5d876c37e1199c10cf9427c99c51303eb9c2
Robert Schäfer <git@roschaefer.de>
2018-08-29 18:50:54 +0200

    Remove obsolete model in faq route

--- frontend/app/routes/faq.js
+++ frontend/app/routes/faq.js
@@ -1,23 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model(){
-    return [
-      'survey',
-      'quality',
-      'impact',
-      'tv-rankings',
-      'representative',
-      'consumption',
-      'binding',
-      'minorities',
-      'finances',
-      'business-model',
-      'who-are-you',
-      'data-privacy',
-      'source-code',
-      'help',
-      'missing'
-    ];
-  }
 });
