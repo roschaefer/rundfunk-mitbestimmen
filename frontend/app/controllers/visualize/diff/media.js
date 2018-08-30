4d2faa11cd6e6d542fea5c01756433deba0258fd
Robert Schäfer <git@roschaefer.de>
2018-08-31 01:18:41 +0200

    Fix ember-copy deprecation
    * Remove own imports of { copy }
    * Update ember-metrics to current git master branch

--- frontend/app/controllers/visualize/diff/media.js
+++ frontend/app/controllers/visualize/diff/media.js
@@ -1,18 +1,17 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
-import { copy } from '@ember/object/internals';
 import { inject as controller } from '@ember/controller';
 
 export default Controller.extend({
   diffController: controller('visualize/diff'),
   mediaChartOptions: computed('diffController.defaultChartOptions', 'model', function() {
-    let chartOptions              = copy(this.get('diffController.defaultChartOptions'), true);
+    let chartOptions              = JSON.parse(JSON.stringify(this.get('diffController.defaultChartOptions'), true));
     chartOptions.chart.height     = this.get('model').get('length') * 30 + 250;
     chartOptions.xAxis.categories = this.get('model').map((medium) => { return medium.get('name')});
     return chartOptions;
   }),
   mediaChartData: computed('diffController.defaultChartData', 'model', function() {
-    let chartData = copy(this.get('diffController.defaultChartData'), true);
+    let chartData = JSON.parse(JSON.stringify(this.get('diffController.defaultChartData'), true));
     chartData[0].data = this.get('model').map((medium) => { return medium.get('total')});
     chartData[1].data = this.get('model').map((medium) => { return medium.get('expected_amount')});
     chartData[2].data = this.get('model').map((medium) => { return medium.get('broadcasts_count')});
