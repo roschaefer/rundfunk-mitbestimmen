7da12942c3de67e0881c62435e6250531e4a0785
Robert Schäfer <git@roschaefer.de>
2018-09-13 18:32:17 +0200

    Fix frontend tests

--- frontend/tests/unit/controllers/login-test.js
+++ frontend/tests/unit/controllers/login-test.js
@@ -5,7 +5,7 @@ import { setupTest } from 'ember-mocha';
 describe('Unit | Controller | login', function() {
   setupTest('controller:login', {
     // Specify the other units that are required for this test.
-    // needs: ['controller:foo']
+    needs: ['service:metrics']
   });
 
   // Replace this with your real tests.
