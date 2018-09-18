07b1395313e0cf49d07dc82d8cda9ca88711139a
faithngetich <faithngetich188@gmail.com>
2018-09-18 14:49:25 +0300

    Follow style guide

--- features/support/wait.rb
+++ features/support/wait.rb
@@ -2,7 +2,7 @@ module Wait
   STEP = 0.1
   def wait_for_ajax
     counter = 0
-    while page.evaluate_script('$.active').to_i > 0
+    while page.evaluate_script('$.active').to_i.positive?
       counter += 1
       sleep(Wait::STEP)
       raise "AJAX request took longer than #{Capybara.default_max_wait_time} seconds." if (Wait::STEP * counter) >= Capybara.default_max_wait_time
