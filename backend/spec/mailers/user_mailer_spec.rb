52bcbc94e00654c50007bede96e2a8e617e2b6d8
faithngetich <faithngetich188@gmail.com>
2018-10-25 18:54:29 +0300

    Test email notification on broadcast creation mailing method

--- backend/spec/mailers/user_mailer_spec.rb
+++ backend/spec/mailers/user_mailer_spec.rb
@@ -35,4 +35,40 @@ RSpec.describe UserMailer, type: :mailer do
       end
     end
   end
+
+  describe 'notify_broadcast_creators_on_broadcast_creation' do
+    let(:user) { create(:user) }
+    let(:mail) { UserMailer.notify_broadcast_creators_on_broadcast_creation }
+    before { create(:broadcast, creator_id: user.id)}
+
+    it 'renders headers' do
+      expect(mail.subject).to eq 'A broadcast has been created'
+      expect(mail.from).to eq ['info@rundfunk-mitbestimmen.de']
+      expect(mail.to).to eq [user.email]
+    end
+
+    it 'includes link to the app' do
+      expect(mail.body.encoded).to match('https://rundfunk-mitbestimmen.de')
+    end
+
+    it 'sends mail' do
+      expect { mail.deliver }.to(change { ActionMailer::Base.deliveries.count }.by(1))
+    end
+
+    context 'user has bad email' do
+      let(:user) { create(:user, email: 'trash@trash-mail.com') }
+
+      it 'won\'t send mail' do
+        expect { mail.deliver }.not_to(change { ActionMailer::Base.deliveries.count })
+      end
+    end
+
+    context 'user has no email' do
+      let(:user) { create(:user, email: nil) }
+
+      it 'won\'t send mail' do
+        expect { mail.deliver }.not_to(change { ActionMailer::Base.deliveries.count })
+      end
+    end
+  end
 end
