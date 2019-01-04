dfed487a96dd34cde4670ff01bf782f690482ca1
Ciré DIA <ciremoussadia@gmail.com>
2019-01-04 16:57:07 +0000

    Removes unnecessary test file
    A file was testing a rake task that has been some commits behind. That caused the test to fail.

--- backend/spec/lib/tasks/build_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'build:test_and_analyze' do
-  include_context 'rake'
-
-  it 'includes all the test and analyze tasks' do
-    expect(test_and_analyze_task.prerequisites).to match_array(%w[rubocop rspec brakeman])
-  end
-end
