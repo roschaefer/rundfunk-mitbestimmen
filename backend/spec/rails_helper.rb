86e32370309dcef8037b4e847760c9289df18780
roschaefer <git@roschaefer.de>
2020-09-09 18:04:55 +0200

    build: Update chrono_model

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -9,6 +9,12 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'vcr'
 require 'json_matchers/rspec'
+# to make amend_period! available
+require 'chrono_model/utilities'
+ActiveRecord::Base.instance_eval do
+  extend ChronoModel::Utilities
+end
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
